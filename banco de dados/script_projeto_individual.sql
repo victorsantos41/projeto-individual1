6d055d34e62399bc1885797fa2e6bbccd8c28f82
victorsantos41 <victor.santos047@sptechschool>
2024-11-24 18:03:52 -0300

    atualizando

--- banco de dados/script_projeto_individual.sql
+++ banco de dados/script_projeto_individual.sql
@@ -1,4 +1,15 @@
+create database MSCS;
 use MSCS;
+
+create table usuario (
+id int primary key auto_increment,
+nome varchar(45),
+email char(255),
+senha varchar(45),
+genero char(10),
+idade int,
+altura float
+);
 
 create table login (
 idLogin int primary key auto_increment,
@@ -6,16 +17,4 @@ email char(255),
 senha varchar(45),
 fkUsuario int
 );
-
-select * from usuario;
-select * from login;
-
-truncate usuario;
-truncate login;
-
-
-
-
-
-
 
